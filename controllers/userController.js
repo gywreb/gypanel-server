236e0a008336cda6f9256df969ec22e549ec921a
gywreb <longnguyenhoang91@gmail.com>
2021-04-29 09:44:26 +0700

    update get current response

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,5 +7,16 @@ exports.getCurrentUser = asyncMiddleware(async (req, res, next) => {
   const { user } = req;
   if (!user) return next(new ErrorResponse(401, "unauthorized"));
   const userInfo = _.omit(user._doc, "password", "_id", "__v");
-  res.json(new SuccessResponse(200, { userInfo }));
+
+  const role = await Role.findById(userInfo.role);
+  if (!role) return next(new ErrorResponse(404, "role not found"));
+
+  res.json(
+    new SuccessResponse(200, {
+      userInfo,
+      role: role.name,
+      routes: role.permissions,
+      methods: role.methods,
+    })
+  );
 });
